e84d7f1ed01b3fe10999d4ef4d1b72f8e2c418eb
PratyakshShiv <pratyakshshivhare@gmail.com>
2023-06-25 22:09:19 +0530

    initial commit

---
+++ components/Logo.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex font-semibold font-mono mt-2 p-4 gap-3">
+      <div className="">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+          />
+        </svg>
+      </div>
+      <div className="text-2xl">Link List</div>
+    </div>
+  );
+};
+
+export default Logo;
